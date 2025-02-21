d9ec7b1470aaaca5aa13689469cc09a32bc75659
Zeeshan Khan <11982876+dropocol@users.noreply.github.com>
2025-02-21 15:21:15 +0500

    Add TypeScript support and GitHub Pages build configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,4 +10,7 @@ export default defineConfig({
   // site: "https://dropocol.github.io",
   trailingSlash: "never",
   integrations: [react(), tailwind()],
+  build: {
+    assets: "assets",
+  },
 });
